87c05659282303a092d2aeccde6de90e0b187a28
Longh33 <lone.gharib@yahoo.se>
2024-11-11 09:25:07 +0100

    added Order Model

---
+++ MaxCinema/Models/OrderRow.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MaxCinema.Models
+{
+    public class OrderRow
+    {
+        [Key]
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+         
+        public int MovieId { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+    }
+}
