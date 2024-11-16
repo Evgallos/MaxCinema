fc16007136e676d8ea2770dea19a76ad3466ca99
Evgallos <evangelos.gallos@gmail.com>
2024-11-16 13:16:50 +0100

    Have change the Customer View, Cinema Library been change to 3 cards per row, Added button Admin and change Customer List as Admin Console Page. I added to our home page dropdown button to left and reveal a list, also changed the color button from succees(green) to danger(red) in Cinema Library, and i changed the color to red Release Year, Director, Price, also i shortet the name director to dir and release to Year because the text was big and destroy the form of our cards in Cinema Library

--- MaxCinema/Services/IMovieService.cs
+++ MaxCinema/Services/IMovieService.cs
@@ -7,8 +7,6 @@ namespace MaxCinema.Services
         public void Create(Movie movie);
         public void Delete(Movie movie);
         public List<Movie> GetListAll();
-        
-
             
     }
 }
